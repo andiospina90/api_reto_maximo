989c446a1fefe1b4c2988ed24c3d9569fa18c48d
Andres Ospina <andi.os90@gmail.com>
2020-04-07 20:14:48 -0500

    Correcion registro de entrenador

--- api_reto_maximo/app/Http/Controllers/RegisterController.php
+++ api_reto_maximo/app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController as BaseController;
 use App\User;
+use App\Trainer;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
